9fb3854f4a5e62f747fd341640216cbf531353fd
hans <hansrajkv@gmail.com>
2025-08-30 18:57:33 -0400

    setup workflow, upstash

---
+++ config/upstash.js
@@ -0,0 +1,10 @@
+import { Client as WorkflowClient } from "@upstash/qstash";
+import {
+  QSTASH_URL,
+  QSTASH_TOKEN,
+} from "./env.js";
+
+export const workflowClient = new WorkflowClient({
+  url: QSTASH_URL,
+  token: QSTASH_TOKEN
+});
